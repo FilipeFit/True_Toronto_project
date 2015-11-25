5cae7806f8768fa2982c34008b2347bb9b4ae62a
FilipeFit <filipe.torqueto@gmail.com>
2015-11-25 12:13:13 -0500

    fix in the user sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -57,4 +57,11 @@ class RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def sign_up_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+   params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
+  end
 end
