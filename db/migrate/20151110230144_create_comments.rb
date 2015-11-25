c8f4e345d2d701e1d7ca5c22738864c087efd4b2
FilipeFit <filipe.torqueto@gmail.com>
2015-11-25 09:55:48 -0500

    Correcao migration comments

--- db/migrate/20151110230144_create_comments.rb
+++ db/migrate/20151110230144_create_comments.rb
@@ -5,7 +5,7 @@ class CreateComments < ActiveRecord::Migration
       t.integer :post_id, null: false
       t.text :body, null: false
       #Somente para commit da correcao
-      t.timestamps, null: false
+      t.timestamps null: false
     end
   end
 end
